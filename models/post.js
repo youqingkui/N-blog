42deb39b697729f50078542a9ac2dce9dc52817f
友情 <youqingkui@qq.com>
2014-05-10 16:37:17 +0800

    第五章，编辑与删除

---
+++ models/post.js
@@ -0,0 +1,205 @@
+var mongodb = require("./db");
+markdown = require('markdown').markdown;
+
+function Post(name, title, post){
+  this.name = name;
+  this.title = title;
+  this.post = post;
+}
+
+module.exports = Post;
+
+Post.prototype.save = function(callback){
+  var date = new Date();
+  var time ={
+    date : date,
+    year : date.getFullYear(),
+    month: date.getFullYear() + "-" + (date.getMonth() +1),
+    day  : date.getFullYear() + "-" + (date.getMonth() +1) + "-" + date.getDate(),
+    minute: date.getFullYear() + "-" + (date.getMonth() +1) + "-" + date.getDate() + " " +
+            date.getHours() + ":" + ((date.getMinutes) < 10 ? '0' + date.getMinutes() : date.getMinutes())
+  }
+  
+  var post = {
+    name : this.name,
+    title: this.title,
+    time : time,
+    post : this.post
+  }
+  
+  mongodb.open(function(err, db){
+    if(err){
+      return callback(err)  
+    }
+    db.collection('posts',function(err, collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      collection.insert(post,{
+        safe: true 
+      }, function(err){
+        mongodb.close();
+        if(err){
+          return callback(err); 
+        }
+        callback(null);
+      });
+      
+    });
+    
+  });
+  
+}
+
+
+Post.getAll = function(name, callback){
+  mongodb.open(function(err, db){
+    if(err){
+      return callback(err); 
+    }
+    db.collection('posts', function(err, collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      var query = {};
+      if(name){
+        query.name = name;
+      }
+      collection.find(query).sort({time: -1}).toArray(function(err, docs){
+        mongodb.close();
+        if(err){
+          return callback(err); 
+        }
+        docs.forEach(function (doc) {
+          doc.post = markdown.toHTML(doc.post);
+        });
+        callback(null, docs);
+        
+      });
+    });
+    
+  }); 
+  
+}
+
+Post.getOne = function(name, day, title, callback){
+  mongodb.open(function(err, db){
+    if(err){
+        return callback(err); 
+    }
+    db.collection('posts', function(err, collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      collection.findOne({
+        "name" : name,
+        "time.day" : day,
+        "title" : title
+      }, function(err, doc){
+        mongodb.close();
+        if(err){
+          return callback(err); 
+        }
+        if(doc){
+          doc.post = markdown.toHTML(doc.post);
+          return callback(null, doc);
+        }
+        return callback(err,null);
+      });
+    });
+    
+  }); 
+}
+
+Post.edit = function(name, day , title, callback){
+  mongodb.open(function(err, db){
+    if(err){
+      return callback(err); 
+    }
+    db.collection('posts', function(err, collection){
+      collection.findOne(
+        {
+          "name" : name,
+          "time.day" :day,
+          "title" : title
+        }, function(err, docs){
+            mongodb.close();
+            if(docs){
+              console.log(docs);
+              return callback(null, docs); 
+            }
+            return callback(err, null);
+        }
+      ); 
+    });
+  });
+}
+
+Post.update = function(name, day, title, post, callback){
+  mongodb.open(function(err, db){
+    if(err){
+      return callback(err); 
+    }
+    db.collection('posts', function(err, collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      collection.update(
+        {
+          "name" : name,
+          "time.day" :day,
+          "title" : title
+        },
+        {
+          "$set":
+          {
+            "post" : post
+            
+          }
+          
+        },
+        function(err){
+          mongodb.close();
+          if(err){
+            return callback(err); 
+          }
+          callback(null);
+        }
+        
+      );
+    });
+  });
+}
+
+Post.remove = function(name, day, title, callback){
+  mongodb.open(function(err, db){
+    if(err){
+      return callback(err); 
+    }
+    db.collection('posts', function(err, collection){
+      if(err){
+        mongodb.close();
+        return callback(err);
+      }
+      collection.remove(
+        {
+          "name" : name,
+          "time.day" : day,
+          "title" : title
+          
+        },function(err){
+          mongodb.close();
+          if(err){
+            return callback(err); 
+          }
+          callback(null);
+        }
+      );
+    });
+  });
+  
+}
---
+++ routes/index.js
@@ -0,0 +1,219 @@
+var express = require('express');
+var router = express.Router();
+var crypto = require('crypto');
+var User   = require('../models/user.js');
+var Post   = require('../models/post.js');
+var fs     = require('fs');
+
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  //res.render('index', { title: 'Express' });
+  Post.getAll(null, function(err, posts){
+    if(err){
+      posts = {} 
+    }
+    res.render('index', {
+      user:req.session.user,
+      posts : posts
+    });
+               
+  });
+  
+});
+
+
+router.get('/reg', function(req, res){
+  res.render('reg', {title:"注册"});
+});
+
+/*用户注册*/
+router.post('/reg', function(req, res){
+  var name = req.body.username.trim();
+  var password = req.body.password.trim();
+  var password_re = req.body["password-repeat"].trim();
+  var email = req.body.email.trim();
+  
+  if(name == ''){
+    var infoMsg = "用户名不能为空";
+    return res.render("reg", {infoMsg: infoMsg});
+  }
+  
+  if(email == ''){
+    var infoMsg = "邮箱为空";
+    return res.render("reg", {infoMsg : infoMsg});
+    
+  }
+  
+  if(password == ''){
+    var infoMsg = "密码为空";
+    return res.render("reg", {infoMsg : infoMsg});
+  }
+  
+  if(password != password_re){
+    var infoMsg = "两次密码不一致";
+    return res.render("reg", {infoMsg : infoMsg});
+  }
+  
+  var md5 = crypto.createHash('md5');
+  var password = md5.update(req.body.password).digest('hex');
+  
+  var newUser = new User({
+    name : name,
+    password : password,
+    email : email
+  });
+  
+  User.get(newUser.name, function(err, user){
+    if(user){
+      var infoMsg = "用户已经存在"; 
+      return res.render("reg", {infoMsg: infoMsg});
+    }
+    newUser.save(function(err, user){
+      if(err){
+        var infoMsg = "注册失败";
+        return res.render("reg", {infoMsg: infoMsg});
+      }
+      req.session.user = user;
+      return res.redirect('/');
+      
+    });
+    
+  });
+                           
+        
+});
+
+
+router.get('/login', function(req, res){
+  res.render('login', {title: "登入"});
+});
+
+router.post('/login', function(req, res){
+  var name = req.body.username.trim();
+  //var password = req.body.password.trim();
+  var md5 = crypto.createHash('md5');
+  var password = md5.update(req.body.password.trim()).digest('hex');
+  User.get(name, function(err, user){
+    if(!user){
+      var infoMsg = "用户不存在"; 
+      return res.render("login", {infoMsg : infoMsg});
+    }
+    if(user.password != password){
+      var infoMsg = "密码错误";
+      return res.render("login", {infoMsg: infoMsg});
+    }
+    req.session.user = user;
+    return res.redirect('/');
+  });
+});
+
+/*发表文章*/
+router.post("/post", function(req, res){
+  var currentUser = req.session.user;
+  console.log(req.body.post);
+  var post = new Post(currentUser.name, req.body.title, req.body.post);
+  post.save(function(err){
+    if(err){
+      console.log(err);
+      return res.redirect('/');
+    }
+    req.session.success = "发表成功";
+    return res.redirect('/');
+  });
+});
+
+
+router.get("/upload", function(req, res){
+  return res.render("upload", {title: "上传图片到文件夹"});
+});
+
+router.post("/upload", function(req, res){
+  console.log(req.files["myfile"]);
+  var target_path = './public/images/' + "okrename.jpg";
+  fs.renameSync(req.files["myfile"].path, target_path);
+  console.log("ok");
+});
+
+
+router.get("/u/:name", function(req, res){
+  User.get(req.params.name, function(err, user){
+    if(!user){
+      req.session.error = "没有这个用户";
+      return res.redirect("/");
+    }
+    Post.getAll(user.name, function(err, posts){
+      if(err){
+        req.session.error = "获取文章出现错误";
+        return res.redirect("/");
+      }
+      return res.render("user", {
+        title : user.name,
+        posts : posts,
+        
+      });
+    });
+  });
+});
+
+router.get("/u/:name/:day/:title", function(req, res){
+  Post.getOne(req.params.name, req.params.day, req.params.title, function(err, posts){
+    if(!posts){
+      req.session.error = "没有找到这片文章";
+      return res.redirect("/");
+    }
+    return res.render("article",{
+      title : posts.title,
+      post : posts
+    });
+  });
+});
+
+router.get("/edit/:name/:day/:title", function(req, res){
+  Post.edit(req.params.name, req.params.day, req.params.title, function(err, post){
+    if(err){
+      req.session.error = "出现错误";
+      return res.redirect("back");
+    }
+    return res.render("edit", 
+      { 
+        title : "编辑",
+        post  : post
+      
+      
+      }               
+    );
+  });
+});
+
+router.post("/edit/:name/:day/:title", function(req, res){
+  var currentUser = req.session.user;
+  Post.update(currentUser.name, req.params.day, req.params.title, req.body.post, function(err){
+    var url = "/u/" + currentUser.name + "/" + req.params.day + "/" + req.params.title;
+    if(err){
+      req.session.error = "更新出现错误";
+      return res.redirect(url);
+    }
+    req.session.success = "更新成功";
+    return res.redirect(url);
+  });
+});
+
+router.get("/delete/:name/:day/:title", function(req, res){
+  var currentUser = req.session.user;
+  Post.remove(currentUser.name, req.params.day, req.params.title, function(err){
+    if(err){
+      req.session.error = "删除出现错误";
+      return res.redirect("back");
+    }
+    req.session.success = "删除成功";
+    return res.redirect("back");
+    
+  });
+});
+
+
+
+
+module.exports = router;
